f40d8f9213d9b9a6ebe11dc182f9b16a04f5439a
Jitendra Choudhary <choudharyj669@gmail.com>
2026-05-08 12:07:47 +0530

    normal commit

---
+++ Day 8/Constant_memberfunc.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Car {
+    private:
+    int speed;
+
+    public:
+    Car() {
+        speed = 120;
+    }
+
+    void showSpeed() const {
+        cout<<"speed is: " << speed << endl;
+    }
+};
+
+int main() {
+    Car c;
+    c.showSpeed();
+
+    return 0;
+}
