307d08124d911940ac87974c8b0d26aba93f7b87
Jitendra Choudhary <choudharyj669@gmail.com>
2026-05-12 12:01:03 +0530

    normal commit

---
+++ Day 12/sumofdigits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int sumofdigits(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return n % 10 + sumofdigits(n / 10);
+}
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    cout << "Sum of digits of " << n << " is: " << sumofdigits(n) << endl;
+}
